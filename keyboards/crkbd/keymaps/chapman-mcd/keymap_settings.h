6ea97e234847290a89b09c872bfaf66b1bec0e2f
Chapman McDaniel <chapman.mcdaniel@icloud.com>
2021-04-27 20:26:42 -0600

    First working build.

---
+++ keyboards/crkbd/keymaps/chapman-mcd/keymap_settings.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+#define U_NP KC_NO // key is not present
+#define U_NA KC_NO // present but not available for use
+#define U_NU KC_NO // available but not used
+
+enum layers { BASE, MBO, MEDR, NAVR, MOUR, NSSL, NSL, FUNL };
+
+#define U_RDO C(KC_Y)
+#define U_PST C(KC_V)
+#define U_CPY C(KC_C)
+#define U_CUT C(KC_X)
+#define U_UND C(KC_Z)
---
+++ keyboards/crkbd/keymaps/chapman-mcd/rules.mk
@@ -0,0 +1,9 @@
+OLED_DRIVER_ENABLE  = yes
+
+# Enable options for miryoku-inspired layout
+MOUSEKEY_ENABLE = yes # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
+AUTO_SHIFT_ENABLE = yes # Auto Shift
+
+# Using elite-c controllers
+BOOTLOADER = qmk-dfu
